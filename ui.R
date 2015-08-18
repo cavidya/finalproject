ef078ed5115a15d0798081fe3e95c3fd77ee3243
serenehxw <sereneho@smrt.com.sg>
2015-08-18 20:59:26 +0800

    FinalProject
    Github which contains all the data and codes for the project

---
+++ ui.R
@@ -0,0 +1,43 @@
+######## function to establish user interface for Shiny app
+## Coursera Final Project
+# created by: Serene Ho
+# 
+
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  
+  headerPanel("Word Text Predictior"),
+  
+  sidebarPanel(
+    
+    textInput("text", label = h3("Input your text"), 
+              value = "Happy Anniversary"),
+    
+    h6(em("Note: program ignore special characters & numbers")),
+    
+    submitButton("Ok")
+    
+  ),
+  
+  mainPanel(
+    h4("Twitter - next word predicted:"),
+    verbatimTextOutput("twitter_next"),
+    h4("Blogs - next word predicted:"),
+    verbatimTextOutput("blogs_next"),
+    h4("News Sites - next word predicted:"),
+    verbatimTextOutput("news_next"),
+    
+    h6("The prediction algoritihm relies on word frequencies in 
+      the English twitter, blogs, and news datasets at:"),
+    
+    h6(a("http://www.corpora.heliohost.org/")),
+    br(),
+    h6("All codes can be found in the Coursera 
+       Capstone folder at:"),
+    h6(a("https://github.com/serenehxw/finalproject"))
+    
+  )
+  
+  
+))
